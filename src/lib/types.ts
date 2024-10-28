1db5e1768261f5042e7f81a0cee30688129abd27
Walassi Silva <walassi.b@gmail.com>
2024-10-28 20:42:53 -0300

    fix: fix validation for max characters

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,9 +2,18 @@ import { z } from "zod";
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 export const formSchema = z.object({
-  nome: z.string().min(3, "O nome deve ter pelo menos 3 letras"),
-  email: z.string().regex(emailRegex, "E-mail inválido"),
-  telefone: z.string().min(11, "O telefone conter DDD mais 9 digitos"),
+  nome: z
+    .string()
+    .min(3, "O nome deve ter pelo menos 3 letras")
+    .max(50, "O nome deve ter no maximo 50 letras"),
+  email: z
+    .string()
+    .max(100, "O e-mail deve ter no maximo 100 letras")
+    .regex(emailRegex, "E-mail inválido"),
+  telefone: z
+    .string()
+    .min(11, "O telefone conter DDD mais 9 digitos")
+    .max(15, "O telefone conter DDD mais 9 digitos"),
   cargo: z.enum([
     "Desenvolvedor Frontend",
     "Desenvolvedor Backend",
@@ -22,8 +31,16 @@ export const formSchema = z.object({
     "Scrum Master",
     "Product Owner",
   ]),
-  linkedin: z.string().optional(),
-  github: z.string().optional(),
+  linkedin: z
+    .string()
+    .url()
+    .max(50, "Link deve conter no máximo 100 caracteres")
+    .optional(),
+  github: z
+    .string()
+    .url()
+    .max(50, "Link deve conter no máximo 100 caracteres")
+    .optional(),
 });
 
 export type FormSchemaType = z.infer<typeof formSchema>;
